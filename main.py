7d7e341af87cfbdd0dd8e635ff3e9b47fb75ce45
apostop <apostop2002@gmail.com>
2023-12-30 02:47:26 +0200

    implement round robin sheduling when process have tha some priority

--- main.py
+++ main.py
@@ -39,12 +39,14 @@ class processQueue:
 
 x1=process("t1",2,0,3)
 x2=process("t2",1,2,2)
-x3=process("t3",7,5,2)
+x3=process("t3",5,5,2)
 x4=process("t4",5,3,3)
-x5=process("t5",5,2,2)
+x5=process("t5",5,3,2)
+x6=process("t6",7,3,2)
 Timequeue = processQueue()
 Priotyqueue = processQueue()
 savequeue = processQueue()
+roundRobin = processQueue()
 
 
 Timequeue.enQueue(x1)
@@ -52,6 +54,7 @@ Timequeue.enQueue(x2)
 Timequeue.enQueue(x3)
 Timequeue.enQueue(x4)
 Timequeue.enQueue(x5)
+Timequeue.enQueue(x6)
 Timequeue.sortByArivalTime()
 Timequeue.printQueue()
 
@@ -72,17 +75,51 @@ for i in range(time):
             x=Timequeue.deQueue()
             Priotyqueue.enQueue(x)
     Priotyqueue.sortByPrioty()
-    if(len(Priotyqueue.Queue)>0):
-        print(Priotyqueue.Queue[0].name, i)
-        Priotyqueue.Queue[0].serviceTime -=1
-        if(Priotyqueue.Queue[0].flagProcess==False):
-            Priotyqueue.Queue[0].processTime=i
-            Priotyqueue.Queue[0].flagProcess=True
+    if(len(Priotyqueue.Queue)>0 or len(roundRobin.Queue)>0):
+        if(len(Priotyqueue.Queue)>0):
+            l=0
+            while (Priotyqueue.Queue[0].priority==Priotyqueue.Queue[l].priority) and (l+1<len(Priotyqueue.Queue)):
+                l+=1
+            if(Priotyqueue.Queue[0].priority==Priotyqueue.Queue[len(Priotyqueue.Queue)-1].priority):
+                l+=1
+        if(l>1 or len(roundRobin.Queue)>0):
+            if(len(roundRobin.Queue)<1):
+                for k in range(l):
+                    x=Priotyqueue.deQueue()
+                    roundRobin.enQueue(x)
+                    q=0
+            if(q<2):
+                q+=1
+                roundRobin.Queue[0].serviceTime-=1
+                print(roundRobin.Queue[0].name, i)
+                if(roundRobin.Queue[0].flagProcess==False):
+                    roundRobin.Queue[0].processTime=i
+                    roundRobin.Queue[0].flagProcess=True
+            else:
+                x=roundRobin.deQueue()
+                roundRobin.enQueue(x)
+                roundRobin.Queue[0].serviceTime-=1
+                print(roundRobin.Queue[0].name, i)
+                if(roundRobin.Queue[0].flagProcess==False):
+                    roundRobin.Queue[0].processTime=i
+                    roundRobin.Queue[0].flagProcess=True
+                q=1
+            if(roundRobin.Queue[0].serviceTime==0):
+                roundRobin.Queue[0].endTime= i
+                x=roundRobin.deQueue()
+                savequeue.enQueue(x)
+                q=0
+        else:
+            print(Priotyqueue.Queue[0].name, i)
+            Priotyqueue.Queue[0].serviceTime -=1
+            if(Priotyqueue.Queue[0].flagProcess==False):
+                Priotyqueue.Queue[0].processTime=i
+                Priotyqueue.Queue[0].flagProcess=True
 
-        if(Priotyqueue.Queue[0].serviceTime == 0):
-            Priotyqueue.Queue[0].endTime= i
-            x=Priotyqueue.deQueue()
-            savequeue.enQueue(x)
+            if(Priotyqueue.Queue[0].serviceTime == 0):
+                Priotyqueue.Queue[0].endTime= i
+                x=Priotyqueue.deQueue()
+                savequeue.enQueue(x)
 
 
 print("*******************")  
